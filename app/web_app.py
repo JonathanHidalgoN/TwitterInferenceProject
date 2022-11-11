c02bb088fc96004f8624bef076dbc17a06a49b87
Jonathan <joni7840850@gmail.com>
2022-11-10 21:31:16 -0600

    Add buttons to search for words

--- app/web_app.py
+++ app/web_app.py
@@ -2,6 +2,7 @@ import streamlit as st
 from tweets_pipeline.database import Database
 from app_options import database_options, stop_words
 from utils import app_functions 
+from utils import text_processing_functions
 #Create a database object and connect to the database
 db = Database(database_options)
 db.connect()
@@ -30,6 +31,14 @@ if option == "Existing users":
     with col1:
         tweets_to_count_words = st.slider("Select the number of tweets to count words", 1000, 10000, 1000)
     with col2:
-        words_to_show = st.slider("Select the number of words to show", 10, 100, 10)
+        words_to_show = st.slider("Select the number of words to show", 10, 60, 10)
     common_words_chart = app_functions.create_common_words_graph(db, selected_user, tweets_to_count_words, stop_words, words_to_show)
-    st.pyplot(common_words_chart, width=500)
+    st.pyplot(common_words_chart, width=500, height=500)
+    ##Centered text
+    st.markdown("<h1 style='text-align: center; Black: red;'>Words search</h1>", unsafe_allow_html=True)
+    col1, col2 = st.columns(2)
+    with col1:
+        word_to_search1 = st.text_input("Insert a word to search for in the tweets of the user", "@")    
+        word_to_search2 = st.text_input("Insert a second word to search for in the tweets of the user", "#")
+        word_to_search3 = st.text_input("Insert a third word to search for in the tweets of the user", "https")
+    
